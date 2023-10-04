a0d787270bde6d9f956b8c010e7c5b5863dc6ef2
goodbadboy12 <146928757+goodbadboy12@users.noreply.github.com>
2023-10-04 09:15:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_08fg71q", "template_yvkrn2f", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
